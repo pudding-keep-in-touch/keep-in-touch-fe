def046613ae23e192481da15a4a68c20de591415
JiyeonMin <ichbinmin2@gmail.com>
2024-09-12 15:56:15 +0000

    ✨ Add received message detail page

---
+++ src/app/message/receive/[detail]/_components/board.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+export default function MessageBoard() {
+  return (
+    <div className='absolute top-[350px] left-1/2 transform -translate-x-1/2 w-full'>
+      <div className='px-6 w-full'>
+        <div className='bg-white bg-opacity-50 backdrop-blur rounded-2xl py-5 px-6 shadow-lg'>
+          <div className='w-full overflow-y-auto max-h-96 text-lg text-[#191F28] leading-relaxed'>
+            내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성내용작성
+          </div>
+          <p className='text-right mt-4 text-lg text-[#3C3C43] font-bold'>
+            From. 홍길동
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/message/receive/[detail]/page.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import MessageBoard from './_components/board'
+import MessageSendButton from './_components/sendButton'
+import MessageShareButton from './_components/shareButton'
+import ReceiveDetailLayout from '@/shared/ui/layouts/ReceiveDetailLayout'
+
+export default function Page() {
+  return (
+    <ReceiveDetailLayout>
+      <div className='relative min-h-screen w-[100%]'>
+        <Image
+          src='/receiveDetailTitle.svg'
+          alt='receive detail title'
+          width={200}
+          height={200}
+          className='absolute top-20 left-1/2 transform -translate-x-1/2'
+        />
+        <div className='w-[32rem]'>
+          <Image src='/receiveDetail.svg' alt='main' width={600} height={800} />
+        </div>
+
+        <MessageBoard />
+
+        <div className='px-6 pb-6 w-full h-fit absolute bottom-1 flex gap-2'>
+          <MessageShareButton />
+          <MessageSendButton />
+        </div>
+      </div>
+    </ReceiveDetailLayout>
+  )
+}
