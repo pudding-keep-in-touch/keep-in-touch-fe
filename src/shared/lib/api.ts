ab523d9cf62db782b06ce63066c4162d528c1bbd
Nam Yeop Lee <namyeop@Nams-MacBook-Pro.local>
2024-09-22 23:14:54 +0900

    :sparkles: Add api calls

---
+++ src/shared/lib/api.ts
@@ -0,0 +1,10 @@
+import apiClient from './apiClient'
+
+export const getHome = async (userId: number) => {
+  try {
+    const response = await apiClient(`/v1/users/${userId}/home`)
+    return response
+  } catch (error) {
+    console.error(error)
+  }
+}
---
+++ src/shared/lib/apiClient.ts
@@ -0,0 +1,34 @@
+const API_BASE_URL = 'http://dev-be.keep-in-touch.me:3000/'
+
+async function apiClient(
+  endpoint: string,
+  options: {
+    headers?: {
+      [key: string]: string
+    }
+  } = {}
+) {
+  const token = localStorage.getItem('token')
+
+  const defaultHeaders = {
+    Authorization: `Bearer ${token}`,
+  }
+
+  const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+    headers: {
+      ...defaultHeaders,
+      ...options.headers,
+    },
+  })
+
+  if (!response.ok) {
+    if (response.status === 401) {
+      window.location.href = '/login'
+    }
+    throw new Error('API 요청 실패')
+  }
+
+  return response.json()
+}
+
+export default apiClient
