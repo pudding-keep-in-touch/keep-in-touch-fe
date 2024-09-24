21d16878399be8ac165897f609202888971a450f
JiyeonMin <ichbinmin2@gmail.com>
2024-09-24 13:43:10 +0000

    ✨query settings related to received message API call

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,22 +1,3 @@
 export interface MainLayoutProps {
   children: React.ReactNode
 }
-
-export interface DmList {
-  id: number
-  senderId: number
-  content: string
-  emotion: {
-    name: string
-    emoji: string
-  }
-  isRead: boolean
-  createdAt: string
-}
-
-export interface HomeResponse {
-  data: {
-    isOwner: boolean
-    dmList: DmList[]
-  }
-}
