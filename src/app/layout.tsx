9af3eb6cd9d8dd23b4f3d36ea85f637a2445acce
JiyeonMin <ichbinmin2@gmail.com>
2024-09-29 19:14:44 +0000

    feat : layout에 오픈그래프 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,17 @@ export default function RootLayout({
   return (
     <html lang='en' className='scrollbar-hide'>
       <head>
+        <meta property='og:title' content='Keep In Touch' />
+        <meta
+          property='og:description'
+          content='너에게 닿기를, Keep In Touch by puddingcamp'
+        />
+        <meta
+          property='og:image'
+          content='https://file.notion.so/f/f/c8c68fae-7ecf-4c58-abfe-1fe57c336005/12d41153-93c9-4689-a31c-499b403e8455/3be10b58-6a4e-4d16-b370-6d46191457a6.png?table=block&amp;id=611e13a0-bb89-4ecf-8a40-8ffbb110fc21&amp;spaceId=c8c68fae-7ecf-4c58-abfe-1fe57c336005&amp;expirationTimestamp=1727625600000&amp;signature=F2yvlWCE70AEsKedCLWJncmJlM_mvqsNGuvT_o292UA&amp;downloadName=2024-09-20_14.10.03.png'
+        />
+        <meta property='og:url' content='https://keep-in-touch.me/' />
+        <meta property='og:type' content='website' />
         <link rel='icon' href='/icon.ico' sizes='any' />
         <Script
           strategy='afterInteractive'
@@ -42,7 +53,6 @@ export default function RootLayout({
           strategy='afterInteractive'
           src='./channeltalk/channeltalk.js'
         />
-
       </head>
 
       <body className='max-w-[32rem] w-full min-h-screen mr-auto ml-auto'>
