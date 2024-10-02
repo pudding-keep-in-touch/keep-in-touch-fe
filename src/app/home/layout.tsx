baa66af2d4c21e97f65c08a407a60cab1f08a27d
JiyeonMin <ichbinmin2@gmail.com>
2024-10-02 10:20:51 +0000

    fix : 로그인 여부 전역에서 체크하여 로그인 상태관리 하에 적용 있도록 홈, 메세지 보내기, 받은 메세지, 프로필 페이지에 AuthProvider 추가

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,11 +1,15 @@
+import AuthProvider from '@/shared/provider/Auth'
+
 interface Props {
   children: React.ReactNode
 }
 
 export default function Layout({ children }: Props) {
   return (
-    <div className='w-full min-h-screen flex flex-col items-center bg-[#F7F7FC]'>
-      {children}
-    </div>
+    <AuthProvider>
+      <div className='w-full min-h-screen flex flex-col items-center bg-[#F7F7FC]'>
+        {children}
+      </div>
+    </AuthProvider>
   )
 }
--- src/app/message/send/[userId]/layout.tsx
+++ src/app/message/send/[userId]/layout.tsx
@@ -4,6 +4,7 @@ import { ChevronLeftIcon } from 'lucide-react'
 import { useParams, usePathname, useRouter } from 'next/navigation'
 import { getVarietyData, MessageVariety } from './[variety]/_utils/varieties'
 import { cn } from '@/shared/lib/utils'
+import AuthProvider from '@/shared/provider/Auth'
 
 interface Props {
   children: React.ReactNode
@@ -23,22 +24,24 @@ export default function Layout({ children }: Props) {
     : 'bg-[#F7F7FC]'
 
   return (
-    <div
-      className={cn(
-        'w-full min-h-screen flex flex-col items-center  pb-16 px-6',
-        makeBgClass
-      )}
-    >
-      <header className='w-full h-[50px] grid grid-cols-3 items-center z-50'>
-        <ChevronLeftIcon
-          className='w-6 h-6 cursor-pointer'
-          onClick={() => router.back()}
-        />
-        <h1 className='text-lg font-semibold text-center text-[#333D4B]'>
-          To. 친구에게
-        </h1>
-      </header>
-      {children}
-    </div>
+    <AuthProvider>
+      <div
+        className={cn(
+          'w-full min-h-screen flex flex-col items-center  pb-16 px-6',
+          makeBgClass
+        )}
+      >
+        <header className='w-full h-[50px] grid grid-cols-3 items-center z-50'>
+          <ChevronLeftIcon
+            className='w-6 h-6 cursor-pointer'
+            onClick={() => router.back()}
+          />
+          <h1 className='text-lg font-semibold text-center text-[#333D4B]'>
+            To. 친구에게
+          </h1>
+        </header>
+        {children}
+      </div>
+    </AuthProvider>
   )
 }
