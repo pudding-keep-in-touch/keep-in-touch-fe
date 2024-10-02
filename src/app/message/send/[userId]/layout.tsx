4c2c70553bec6383282ecf5fc043cf661c10ed23
JiyeonMin <ichbinmin2@gmail.com>
2024-10-02 17:21:19 +0000

    fix : preview 페이지에서만 header의 title을 보여주지 않도록 처리함

--- src/app/message/send/[userId]/layout.tsx
+++ src/app/message/send/[userId]/layout.tsx
@@ -2,7 +2,7 @@
 
 import { ChevronLeftIcon } from 'lucide-react'
 import { useParams, usePathname, useRouter } from 'next/navigation'
-import { getVarietyData, MessageVariety } from './[variety]/_utils/varieties'
+import { MessageVariety } from './[variety]/_utils/varieties'
 import { cn } from '@/shared/lib/utils'
 import AuthProvider from '@/shared/provider/Auth'
 
@@ -12,7 +12,7 @@ interface Props {
 
 export default function Layout({ children }: Props) {
   const router = useRouter()
-  const params = useParams<{ variety: MessageVariety }>()
+  const params = useParams<{ variety: MessageVariety; userId: string }>()
   const pathname = usePathname()
 
   const makeBgClass = pathname.endsWith('/preview')
@@ -24,24 +24,27 @@ export default function Layout({ children }: Props) {
     : 'bg-[#F7F7FC]'
 
   return (
-    <AuthProvider>
-      <div
-        className={cn(
-          'w-full min-h-screen flex flex-col items-center  pb-16 px-6',
-          makeBgClass
-        )}
-      >
-        <header className='w-full h-[50px] grid grid-cols-3 items-center z-50'>
-          <ChevronLeftIcon
-            className='w-6 h-6 cursor-pointer'
-            onClick={() => router.back()}
-          />
+    // <AuthProvider>
+    <div
+      className={cn(
+        'w-full min-h-screen flex flex-col items-center pb-16 px-6',
+        makeBgClass
+      )}
+    >
+      <header className='w-full h-[50px] grid grid-cols-3 items-center z-50'>
+        <ChevronLeftIcon
+          className='w-6 h-6 cursor-pointer'
+          onClick={() => router.back()}
+        />
+
+        {!pathname.endsWith('/preview') && (
           <h1 className='text-lg font-semibold text-center text-[#333D4B]'>
-            To. 친구에게
+            {`To. ${params.userId}에게`}
           </h1>
-        </header>
-        {children}
-      </div>
-    </AuthProvider>
+        )}
+      </header>
+      {children}
+    </div>
+    // </AuthProvider>
   )
 }
