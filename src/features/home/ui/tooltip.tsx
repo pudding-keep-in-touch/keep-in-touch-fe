f0c73dd1ea431fb57cc1559d5bf306d214d543c2
JiyeonMin <ichbinmin2@gmail.com>
2024-11-20 17:39:03 +0000

    fix : Tooltip 스타일 변경 및 문구 수정

--- src/features/home/ui/tooltip.tsx
+++ src/features/home/ui/tooltip.tsx
@@ -8,11 +8,11 @@ interface TooltipProps {
 
 export default function Tooltip({ children }: TooltipProps) {
   return (
-    <div className='flex items-center justify-center mt-[46px] w-full h-fit flex-col gap-4'>
-      <div className='relative inline-block'>
-        <div className='bg-black text-white text-sm rounded-full py-3 px-4 h-[46px] relative'>
-          <p className='line-clamp-1 text-base'>
-            🔗 내 링크를 받은 친구가 쪽지를 쓸 수 있어요!
+    <div className='group relative flex items-center justify-center mt-[46px] w-full h-fit flex-col gap-4'>
+      <div className='absolute top-[-60px] hidden group-hover:flex flex-col items-center gap-4'>
+        <div className='bg-black rounded-full py-3 px-[41px] h-[46px] relative'>
+          <p className='line-clamp-1 text-base text-[13px] text-white'>
+            🔗 이 질문 하나만 공유할 수 있어요!
           </p>
           <div className='absolute w-3 h-3 bg-black transform rotate-45 left-1/2 -translate-x-1/2 -bottom-1.5'></div>
         </div>
