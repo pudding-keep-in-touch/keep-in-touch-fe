be212d64d195a79b839e6533544a2d9b4d3ae204
JiyeonMin <ichbinmin2@gmail.com>
2024-11-20 17:38:31 +0000

    style : 네비게이션 바의 스타일 변경

--- src/shared/components/nav.tsx
+++ src/shared/components/nav.tsx
@@ -1,6 +1,6 @@
 export const Nav = () => {
   return (
-    <div className='w-full h-[84px] bg-gray-1 shadow-md bottom-0 flex justify-between items-center px-[120px] absolute z-10'>
+    <div className='w-full h-[84px] bg-gray-1 shadow-md bottom-0 flex justify-center items-center px-[10px] absolute z-10 gap-[97px]'>
       <button className='flex flex-col items-center text-gray-500 gap-1'>
         <img src='/nav_home.svg' className='w-6 h-6' alt='Home' />
         <span className='text-[11px] font-semibold text-[#6B7684]'>홈</span>
