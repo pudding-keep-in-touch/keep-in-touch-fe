97182f3b3063eafc3ad245285ccc0009077f624d
JiyeonMin <ichbinmin2@gmail.com>
2024-11-20 17:37:41 +0000

    style : home header의 스타일 변경

--- src/shared/components/homeHeader.tsx
+++ src/shared/components/homeHeader.tsx
@@ -4,14 +4,16 @@ interface HomeHeaderProps {
 
 export const HomeHeader = ({ isVisible }: HomeHeaderProps) => {
   return (
-    <header
-      className={`flex justify-between items-center w-full h-[60px] px-[19px] z-10 ${isVisible ? 'bg-transparent' : 'bg-white'}`}
-    >
-      <div />
-      <img className='translate-x-1' src='/header_title.svg' />
-      <button>
-        <img src='/header_more.svg' />
-      </button>
-    </header>
+    <div className='absolute top-0 left-0 w-full z-10'>
+      <header
+        className={`flex-grow flex justify-between items-center w-full h-[60px] px-[19px] ${isVisible ? 'bg-transparent' : 'bg-white'}`}
+      >
+        <div />
+        <img className='translate-x-1' src='/header_title.svg' />
+        <button>
+          <img src='/header_more.svg' />
+        </button>
+      </header>
+    </div>
   )
 }
