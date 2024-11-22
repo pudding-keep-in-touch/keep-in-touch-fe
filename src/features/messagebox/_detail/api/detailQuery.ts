3c59e540ea6165b8851b978b088ccfd0cb178089
jieunw <jieunwang00@gmail.com>
2024-11-22 07:27:29 +0000

    feat: add query for fetching messages 	* integrate mockdata for testing message query

--- src/features/messagebox/_detail/api/detailQuery.ts
+++ src/features/messagebox/_detail/api/detailQuery.ts
@@ -1,23 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
-import { useGetMessageListProps } from '@/features/messagebox/model/messagebox.types'
+import {
+  MessageDetail,
+  MessageResponse,
+  useGetMessageListProps,
+} from '@/features/messagebox/model/messagebox.types'
 import { baseQuery } from '@/shared/api/baseQuery'
 
-interface useGetMessageDetailProps {
-  messageId: number
-}
-interface MessageResponse {
-  received_message_count: number
-  nextCursor: string | null // nextCursor 이거 타입 확인 필요, Date로 변경 가능성 O
-  messageList: Array<{
-    messageId: number
-    receiverId: number
-    receiverNickname: string
-    content: string
-    createdAt: string
-  }>
-}
-
-const mockData = {
+const receivedMockData = {
   received_message_count: 3,
   nextCursor: '2024-11-15T08:08:38.654Z',
   messageList: [
@@ -45,6 +34,37 @@ const mockData = {
   ],
 }
 
+const sentMockData = {
+  sentMessageCount: 3,
+  nextCursor: '2024-11-15T08:08:38.654',
+  messageList: [
+    {
+      messageId: 1,
+      receiverId: 1,
+      receiverNickname: 'Jisu Kim',
+      content: '안녕, 너가 토이 프로젝트를 배포까지 하다니 진짜 대단하다..!!',
+      status: 'normal',
+      createdAt: '2024-09-02',
+    },
+    {
+      messageId: 2,
+      receiverId: 1,
+      receiverNickname: 'Jisu Kim',
+      content: '안녕, 너가 토이 프로젝트를 배포까지 하다니 진짜 대단하다..!!',
+      status: 'normal',
+      createdAt: '2024-09-02',
+    },
+    {
+      messageId: 3,
+      receiverId: 1,
+      receiverNickname: 'Jisu Kim',
+      content: '안녕, 너가 토이 프로젝트를 배포까지 하다니 진짜 대단하다..!!',
+      status: 'normal',
+      createdAt: '2024-09-02',
+    },
+  ],
+}
+
 // 목 데이터 테스트
 export const useGetMessageList = ({
   userId,
@@ -54,14 +74,15 @@ export const useGetMessageList = ({
   order,
 }: useGetMessageListProps) => {
   return useQuery<MessageResponse, Error>({
-    queryKey: ['getMessageList', userId, type],
+    queryKey: ['getMessageList', userId, type, cursor, limit, order],
     queryFn: async () => {
-      return mockData
+      return receivedMockData
     },
     enabled: true,
   })
 }
 
+// 본 테스트
 // export const useGetMessageList = ({
 //   userId,
 //   type,
@@ -91,22 +112,83 @@ export const useGetMessageList = ({
 //     enabled: true,
 //   })
 // }
+const messageIdMockData = {
+  messageId: 1,
+  type: 'received',
+  receiverId: 11,
+  receiverNickname: '제인',
+  content: '질문에 대한 상세 대답',
+  question: {
+    questionId: 1,
+    content: '상세임 여기',
+  },
+  reactions: [
+    {
+      reactionId: 1,
+      content: 'ㄱㅅ~',
+      type: '감사',
+      emoji: '🧐',
+    },
+    {
+      reactionId: 2,
+      content: '미안해',
+      type: '사과',
+      emoji: '🧐',
+    },
+  ],
+  status: 'normal',
+  createdAt: '2024-09-02',
+}
+// const messageIdMockData = {
+//   messageId: 1,
+//   type: 'received',
+//   receiverId: 11,
+//   receiverNickname: 'Jisu Kim',
+//   content: '질문에 대한 상세 대답',
+//   question: {
+//     questionId: 1,
+//     content: '상세임 여기',
+//   },
+//   reactions: [
+//     {
+//       reactionId: 1,
+//       content: '감사감사합니다~',
+//       type: '감사',
+//       emoji: '🧐',
+//     },
+//     {
+//       reactionId: 2,
+//       content: '미안해',
+//       type: '사과',
+//       emoji: '🧐',
+//     },
+//   ],
+//   status: 'normal',
+//   createdAt: '2024-09-02',
+// }
 
 // mock 데이터 테스트
-export const useGetMessageDetail = ({
-  messageId,
-}: useGetMessageDetailProps) => {
-  return useQuery({
+//
+export const useGetMessageDetail = ({ messageId }: { messageId: number }) => {
+  return useQuery<MessageDetail, Error>({
     queryKey: ['getDetailMessage', messageId],
-    // queryFn: async () => {
-    //   const { data } = await baseQuery.get(`/v2/messages/${messageId}`, {
-    //     headers: {
-    //       Authorization: `Bearer ${localStorage.getItem('keep_in_touch_token')}`,
-    //     },
-    //   })
-    //   return data
     queryFn: async () => {
-      return mockData
+      return messageIdMockData // mockData 반환
     },
   })
 }
+
+// 본 테스트
+// export const useGetMessageDetail = ({ messageId }: MessageDetail) => {
+//   return useQuery({
+//     queryKey: ['getDetailMessage', messageId],
+//     queryFn: async () => {
+//       const { data } = await baseQuery.get(`/v2/messages/${messageId}`, {
+//         headers: {
+//           Authorization: `Bearer ${localStorage.getItem('keep_in_touch_token')}`,
+//         },
+//       })
+//       return data
+//     },
+//   })
+// }
