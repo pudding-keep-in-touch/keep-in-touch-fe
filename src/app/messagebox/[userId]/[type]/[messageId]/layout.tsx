b58c7074d5bef05a743b64c35cb8552c229c7882
jieunw <jieunwang00@gmail.com>
2024-11-23 08:08:34 +0000

    feat: report page UI

--- src/app/messagebox/[userId]/[type]/[messageId]/layout.tsx
+++ src/app/messagebox/[userId]/[type]/[messageId]/layout.tsx
@@ -1,13 +1,17 @@
 'use client'
-import React from 'react'
 import { ChevronLeftIcon } from 'lucide-react'
 import { usePathname, useRouter } from 'next/navigation'
 import { cn } from '@/shared/utils/emotionVariety'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({
+  children,
+  modal,
+}: {
+  modal: React.ReactNode
+  children: React.ReactNode
+}) {
   const router = useRouter()
   const pathname = usePathname()
-
   const makeBgClass = pathname.endsWith('/reaction')
     ? 'bg-[#FFFFFF]'
     : 'bg-messageDetail'
@@ -19,16 +23,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         makeBgClass
       )}
     >
-      <header className='w-full h-[50px] flex justify-between items-center z-50 '>
-        <ChevronLeftIcon
-          className='w-6 h-6 cursor-pointer'
-          onClick={() => router.back()}
-        />
-        <button type='button'>
-          <img src='/header_more.svg' />
-        </button>
-      </header>
       {children}
+      {modal}
     </div>
   )
 }
--- src/app/messagebox/[userId]/[type]/[messageId]/page.tsx
+++ src/app/messagebox/[userId]/[type]/[messageId]/page.tsx
@@ -1,12 +1,53 @@
+'use client'
 import { MessageType } from '@/features/messagebox/_detail/model/messagebox.types'
 import MessageDetail from '@/features/messagebox/ui/MessageDetail'
+import React, { useState } from 'react'
+import { ChevronLeftIcon } from 'lucide-react'
+import { cn } from '@/shared/utils/emotionVariety'
+import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 export default function Page({
   params: { userId, type, messageId },
 }: {
   params: { userId: number; type: MessageType; messageId: number }
 }) {
+  const router = useRouter()
+  const [isOpen, setIsOpen] = useState(false)
+  const openModal = () => {
+    setIsOpen(true)
+  }
+
   return (
-    <MessageDetail userId={userId} messageId={messageId} messageType={type} />
+    <div className='w-full h-full'>
+      <div className='flex justify-between h-full'>
+        <header className='w-full h-[50px] flex justify-between items-center z-50 '>
+          <ChevronLeftIcon className='w-6 h-6 cursor-pointer' />
+          <div className='flex flex-col items-end h-full'>
+            <button type='button' onClick={openModal}>
+              <img src='/header_more.svg' />
+            </button>
+            {isOpen ? (
+              <div className='relative bg-black w-[100px] h-[56px] rounded-2xl text-white'>
+                <button
+                  onClick={() =>
+                    router.push(
+                      `/messagebox/${userId}/${type}/${messageId}/singo`
+                    )
+                  }
+                >
+                  신고하기
+                </button>
+                {/* <Link>숨기기</Link> */}
+              </div>
+            ) : (
+              ''
+            )}
+          </div>
+        </header>
+      </div>
+      <MessageDetail userId={userId} messageId={messageId} messageType={type} />
+    </div>
   )
 }
