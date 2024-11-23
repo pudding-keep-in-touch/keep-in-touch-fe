3502e69c3f72a7c47fbadd5b93befe57319630bf
JiyeonMin <ichbinmin2@gmail.com>
2024-11-23 16:10:40 +0900

    fix : homeHeader 에 isHome을 props로 받도록 추가하고, isHome을 기준으로 스타일 분기처리

--- src/shared/components/homeHeader.tsx
+++ src/shared/components/homeHeader.tsx
@@ -1,16 +1,32 @@
+import { ChevronLeftIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
 interface HomeHeaderProps {
   isVisible: boolean
+  isHome: boolean
 }
 
-export const HomeHeader = ({ isVisible }: HomeHeaderProps) => {
+export const HomeHeader = ({ isVisible, isHome }: HomeHeaderProps) => {
+  const router = useRouter()
+
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
       <header
         className={`flex-grow flex justify-between items-center w-full h-[60px] px-[19px] ${isVisible ? 'bg-transparent' : 'bg-white'}`}
       >
-        <div />
-        <img className='translate-x-1' src='/header_title.svg' />
-        <button>
+        {isHome ? (
+          <div />
+        ) : (
+          <ChevronLeftIcon
+            className='w-6 h-6 cursor-pointer'
+            onClick={() => router.back()}
+          />
+        )}
+        <img
+          className={`${isHome && 'translate-x-3'}`}
+          src='/header_title.svg'
+        />
+        <button className='w-6'>
           <img src='/header_more.svg' />
         </button>
       </header>
