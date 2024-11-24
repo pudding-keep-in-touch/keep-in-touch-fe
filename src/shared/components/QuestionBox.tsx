61893cb9d4ab0fed4005091b360a99719a39ba70
lunashp <hijnshp@gmail.com>
2024-11-25 00:50:23 +0900

    feat: QuestionBox 컴포넌트 자유질문 영역 이미지로 수정

--- src/shared/components/QuestionBox.tsx
+++ src/shared/components/QuestionBox.tsx
@@ -1,12 +1,11 @@
-import { useRandomDescriptionQuery } from '@/features/questions/hooks/query/useRandomDescriptionQuery'
 import React from 'react'
+import Image from 'next/image'
 
 type QuestionBoxVariant = 'default' | 'custom'
 
 interface QuestionBoxProps {
   questionId?: string
   content?: string
-  description?: string // 자유질문의 설명 추가
   variant?: QuestionBoxVariant
   onClick?: (questionId: string, content: string) => void
 }
@@ -17,7 +16,6 @@ const QuestionBox: React.FC<QuestionBoxProps> = ({
   variant = 'default',
   onClick,
 }) => {
-  const { data: randomDescription } = useRandomDescriptionQuery()
   const handleClick = () => {
     if (onClick) {
       onClick(questionId || '', content || '')
@@ -26,19 +24,32 @@ const QuestionBox: React.FC<QuestionBoxProps> = ({
 
   return (
     <div
-      className='bg-white rounded-lg border-[1px] border-[#CCE8F4] rounded-[12px] flex flex-col items-center cursor-pointer'
+      className='bg-white rounded-lg border-[1px] border-[#CCE8F4] overflow-hidden cursor-pointer'
       onClick={handleClick}
     >
-      <h3 className='text-sm font-semibold text-[#333D4B] bg-gray-100 w-full text-center py-3 rounded-md'>
+      {/* 상단 텍스트 */}
+      <div className='text-sm font-semibold text-[#333D4B] bg-gray-100 text-center py-3 rounded-t-md'>
         {variant === 'custom' ? '자유질문' : '질문'}
-      </h3>
-      <p
-        className='text-sm text-[#6B7684] text-center mt-3 mb-3'
-        style={{ whiteSpace: 'pre-line' }}
-      >
-        {variant === 'custom' ? randomDescription : content}
-      </p>
+      </div>
+
+      {/* 하단 내용 */}
+      {variant === 'custom' ? (
+        <Image
+          src='/freeQuestion.svg'
+          alt='자유질문 이미지'
+          width={340}
+          height={340}
+        />
+      ) : (
+        <p
+          className='text-sm text-[#6B7684] text-center mt-3 mb-3'
+          style={{ whiteSpace: 'pre-line' }}
+        >
+          {content}
+        </p>
+      )}
     </div>
   )
 }
+
 export default QuestionBox
