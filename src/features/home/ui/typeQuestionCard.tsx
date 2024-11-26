33940c2a41b7783a50cb24480e74ec00a1283da4
JiyeonMin <ichbinmin2@gmail.com>
2024-11-27 00:10:52 +0900

    fix : 타입별 질문 카드 문구 수정

--- src/features/home/ui/typeQuestionCard.tsx
+++ src/features/home/ui/typeQuestionCard.tsx
@@ -1,31 +1,18 @@
-import { useRouter } from 'next/navigation'
 import React from 'react'
 
 interface QuestionsCardProps {
-  questionId?: number
-  userId: number
   isHome: boolean
 }
 
-export const TypeQuestionCard = ({
-  questionId,
-  userId,
-  isHome,
-}: QuestionsCardProps) => {
-  const router = useRouter()
-
-  const onClickMoveHome = () => {
-    // TODO : 자유 질문이면 링크 이동처리
-    router.push(`/home/${userId}/question/free`)
-  }
-
+export const TypeQuestionCard = ({ isHome }: QuestionsCardProps) => {
   return (
     <div
-      onClick={onClickMoveHome}
       className={`flex-col justify-start w-full bg-opacity-30 border border-[#D0E4FF] rounded-2xl overflow-hidden mt-[10px] relative  ${isHome ? 'h-[186px] cursor-pointer' : 'h-fit'}`}
     >
       <div className='flex justify-center items-center w-full h-[44px] bg-gray-1'>
-        <h3 className='text-gray-4 text-sm font-semibold'>타입별 마음 받기</h3>
+        <h3 className='text-gray-4 text-[14px] font-semibold'>
+          질문 없이도 퐁을 받을 수 있어요
+        </h3>
       </div>
       <div className='flex justify-center items-center w-full bg-white overflow-hidden'>
         {/* 이미지 */}
