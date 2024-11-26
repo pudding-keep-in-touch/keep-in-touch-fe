e73e2b90d346c2a3e333b36ef4e2fb02d1dce772
JiyeonMin <ichbinmin2@gmail.com>
2024-11-26 20:43:59 +0900

    feat : 홈의 내 질문리스트 전체 공유하기 기능 추가

--- src/features/home/ui/questionListHeader.tsx
+++ src/features/home/ui/questionListHeader.tsx
@@ -1,15 +1,42 @@
 import clsx from 'clsx'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+import toast from 'react-hot-toast'
 
 interface QuestionListHeaderProps {
   className?: string
+  userId: string
 }
 
 export const QuestionListHeader = React.forwardRef<
   HTMLDivElement,
   QuestionListHeaderProps
->(({ className }, ref) => {
+>(({ className, userId }, ref) => {
+  const pathname = usePathname()
+
+  function shareOnUrl() {
+    const contentToCopy = `https://dev-fe.keep-in-touch.me${pathname}`
+
+    const urlArea = document.createElement('textarea')
+    document.body.appendChild(urlArea)
+    urlArea.value = contentToCopy
+    urlArea.select()
+    document.execCommand('copy')
+    document.body.removeChild(urlArea)
+
+    toast(
+      <div className='w-full flex items-center space-x-3 relative justify-center'>
+        <img
+          src='/icon-check-fill.svg'
+          alt='check icon fill'
+          className='w-5 h-5'
+        />
+        <p>링크가 복사되었습니다.</p>
+      </div>
+    )
+  }
+
   return (
     <div
       ref={ref}
@@ -21,7 +48,10 @@ export const QuestionListHeader = React.forwardRef<
       <h3 className='font-semibold text-[18px] tracking-[-0.0625rem]'>
         내 질문 리스트
       </h3>
-      <button className='flex justify-center items-center flex-shrink-0  py-[3px] px-[9px] w-[110px] bg-[#35B6FF] bg-opacity-30 rounded-3xl gap-[9px]'>
+      <button
+        onClick={shareOnUrl}
+        className='flex justify-center items-center flex-shrink-0  py-[3px] px-[9px] w-[110px] bg-[#35B6FF] bg-opacity-30 rounded-3xl gap-[9px]'
+      >
         <div>
           <span className='text-[#0788D1] font-bold text-[12.98px] tracking-[-0.045rem] leading-[140%]'>
             전체 공유하기
