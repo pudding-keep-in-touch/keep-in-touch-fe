228fcc2282048646911e575e896fb6c8e440be37
JiyeonMin <ichbinmin2@gmail.com>
2024-11-27 00:09:19 +0900

    remove : 타입별 질문 상세 페이지 삭제

--- src/app/home/[useId]/question/free/page.tsx
+++
@@ -1,9 +0,0 @@
-import QuestionDetail from '@/features/home/questionDetail'
-
-export default function QuestionDetailPage({
-  params: { questionId, userId },
-}: {
-  params: { questionId: number; userId: number }
-}) {
-  return <QuestionDetail questionId={questionId} userId={userId} />
-}
