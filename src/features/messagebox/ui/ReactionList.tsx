4fff9a8fcf6a15727edae94f3577f10c5d85ecaf
jieunw <jieunwang00@gmail.com>
2024-11-27 18:27:48 +0900

    feat: 반응 보내기 페이지 재구성

--- src/features/messagebox/ui/ReactionList.tsx
+++ src/features/messagebox/ui/ReactionList.tsx
@@ -1,140 +1,17 @@
 import { EmojiProps } from '@/features/messagebox/model/messagebox.types'
 
-const emotion = {
-  감사: [
-    {
-      id: 1,
-      emoji: '🙂',
-      name: '고마워',
-    },
-    {
-      id: 2,
-      emoji: '🥰',
-      name: '덕분이야',
-    },
-    {
-      id: 3,
-      emoji: '😘',
-      name: '최고야',
-    },
-    {
-      id: 4,
-      emoji: '🥺',
-      name: '감동이야',
-    },
-    {
-      id: 5,
-      emoji: '😉',
-      name: '너밖에 없어',
-    },
-  ],
-  사과: [
-    {
-      id: 1,
-      emoji: '🙂',
-      name: '내가 더 잘할게',
-    },
-    {
-      id: 2,
-      emoji: '🥰',
-      name: '미안해',
-    },
-    {
-      id: 3,
-      emoji: '😘',
-      name: '만나서 얘기하자',
-    },
-    {
-      id: 4,
-      emoji: '🥺',
-      name: '반성할게',
-    },
-    {
-      id: 5,
-      emoji: '😉',
-      name: '다시는 그러지 않을게',
-    },
-  ],
-  응원: [
-    {
-      id: 1,
-      emoji: '🙂',
-      name: '화이팅',
-    },
-    {
-      id: 2,
-      emoji: '🥰',
-      name: '멋있어',
-    },
-    {
-      id: 3,
-      emoji: '😘',
-      name: '고생 많았어',
-    },
-    {
-      id: 4,
-      emoji: '🥺',
-      name: '응원할게',
-    },
-    {
-      id: 5,
-      emoji: '😉',
-      name: '행운을 빌어',
-    },
-  ],
-  화해: [
-    {
-      id: 1,
-      emoji: '🙂',
-      name: '화해하자',
-    },
-    {
-      id: 2,
-      emoji: '🥰',
-      name: '다음에는 그러지 마',
-    },
-    {
-      id: 3,
-      emoji: '😘',
-      name: '만나서 얘기하자',
-    },
-    {
-      id: 4,
-      emoji: '🥺',
-      name: '나한테 잘해',
-    },
-    {
-      id: 5,
-      emoji: '😉',
-      name: '한번만 봐줄게',
-    },
-  ],
-}
-
-const ReactionList: React.FC<EmojiProps> = ({ category }) => {
-  const items = emotion[category] || []
+const ReactionList = ({ data }: { data: EmojiProps }) => {
   return (
-    <>
-      <div className='w-full flex flex-col py-[20px] h-full'>
-        <div className='w-full pb-[20px] flex items-center gap-[10px]'>
-          <h2 className='text-[#1F1F1F] font-semibold leading-none'>
-            {category}
-          </h2>
-        </div>
-        <div className='w-full flex flex-wrap gap-[8px]'>
-          {items.map((e) => (
-            <div key={e.id} className='flex-none'>
-              <div className='w-full p-[12px] h-[35px] bg-white rounded-[60px] flex items-center cursor-pointer border-[0.5px] hover:bg-[#35B6FF] hover:bg-opacity-40 hover:border-[#35B6FF]  border-[#C5C5C5] gap-[4px]'>
-                <div>{e.emoji}</div>
-                <div className='w-full flex text-[15px] font-semibold leading-[68.1%] tracking-[-2%]'>
-                  {e.name}
-                </div>
-              </div>
-            </div>
-          ))}
+    <div className='w-full flex h-full'>
+      <div className='flex-none'>
+        <div className='w-full p-[12px] h-[35px] text-[21px] bg-white rounded-[60px] flex items-center cursor-pointer border-[0.5px] hover:bg-[#35B6FF] hover:bg-opacity-40 hover:border-[#35B6FF]  border-[#C5C5C5] gap-[4px]'>
+          {data.emoji}
+          <div className='w-full flex text-[15px] font-semibold leading-[68.1%] tracking-[-2%]'>
+            {data.content}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/features/messagebox/ui/ReactionPage.tsx
+++ src/features/messagebox/ui/ReactionPage.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/shared/components/Button'
-import { DataType } from '@/features/messagebox/model/messagebox.types'
+import { EmojiProps } from '@/features/messagebox/model/messagebox.types'
 import { MessageType } from '@/features/messagebox/_detail/model/messagebox.types'
 import ReactionList from '@/features/messagebox/ui/ReactionList'
+import { useGetEmoji } from '../_detail/api/detailQuery'
 
 export default function ReactionPage({
   messageType,
@@ -14,20 +15,52 @@ export default function ReactionPage({
   userId: number
   messageId: number
 }) {
-  const List: Array<keyof DataType> = ['감사', '사과', '응원', '화해']
+  const lists = ['감사', '사과', '응원', '화해']
   const router = useRouter()
   const onSubmit = () => {
     // 데이터 보내는 로직 추가
     router.push(`/messagebox/${userId}/${messageType}/${messageId}`)
   }
 
+  const { data, error, isLoading } = useGetEmoji()
+
+  if (isLoading) return <div>임시 로딩중..</div>
+  if (error) return <div>Error fetching emojis.</div>
+  if (!data) return null
+
+  // 데이터 그룹화
+  const groupedData: Record<EmojiProps['type'], EmojiProps[]> = lists.reduce(
+    (acc, type) => {
+      acc[type] = [] // 초기화
+      return acc
+    },
+    {} as Record<EmojiProps['type'], EmojiProps[]>
+  )
+
+  data.forEach((item) => {
+    if (groupedData[item.type]) {
+      groupedData[item.type].push(item) // 해당 유형의 배열에 추가
+    }
+  })
+
   return (
     <div className='w-full h-full'>
-      {List.map((title) => (
-        <div key={title} className='border-b-[6px] border-b-[#F6F7FC] w-full'>
-          <ReactionList category={title} />
+      {lists.map((type) => (
+        <div
+          key={type}
+          className='w-full flex flex-col py-[20px] h-full border-b-[6px] border-b-gray-1'
+        >
+          <h2 className='text-base font-semibold tracking-[-0.75px]'>{type}</h2>
+          <div className='flex w-full flex-wrap gap-2 pt-[20px]'>
+            {groupedData[type].map((item) => (
+              <div key={item.templateId}>
+                <ReactionList data={item} />
+              </div>
+            ))}
+          </div>
         </div>
       ))}
+
       <Button
         type='button'
         onClick={onSubmit}
