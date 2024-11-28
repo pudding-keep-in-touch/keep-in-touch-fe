b32dd8fcf98a7573f75ec9aef167537c423e6b3a
jieunw <jieunwang00@gmail.com>
2024-11-28 13:53:36 +0900

    feat: fetching emoji values 	* fix: /reaction header css error 	* feat: add Toast alert

--- src/features/messagebox/model/messagebox.types.ts
+++ src/features/messagebox/model/messagebox.types.ts
@@ -1,74 +1,48 @@
-// import { MessageType } from "@/shared/types/common.types"
 import { MessageType } from '../_detail/model/messagebox.types'
 
 export type MessageBoxType = {
   messageId: number
 }
-// export type ExtendedMessageType = MessageType | 'reaction'
-
-type questionContainer = {
-  questionId: number
-  content: string
-}
-type emotionContainer = {
-  emotionId: number
-  name: string
-  emoji: string
-}
-
-type reactionContainer = {
-  reactionId: number
-  content: string
-  type: string
-  emoji: string
-}
 
 // 받은 쪽지 상세 조회 타입
-// export interface MessageDetail {
-//   messageId: number
-//   type: string
-//   receiverId: number
-//   receiverNickname: string
-//   content: string
-//   emotion?: emotionContainer
-//   question: questionContainer
-//   reactions: reactionContainer[]
-//   status?: string
-//   createdAt: string
-// }
-
 export interface MessageDetail {
-  messageId: number
-  type: string
-  receiverId: number
+  status: number
+  message: string
+  data: {
+    messageId: string
+    type: string
+    receiverId: string
+    receiverNickname: string
+    content: string
+    question: {
+      questionId: string
+      content: string
+    }
+    reactions?: {
+      reactionId: string
+      content: string
+      type: string
+      emoji: string
+    }[]
+    status?: string
+    createdAt: Date | null
+  }
+}
+export type Message = {
+  messageId: string
+  receiverId: string
   receiverNickname: string
   content: string
-  question: {
-    questionId: number
-    content: string
-  }
-  reactions: {
-    reactionId: number
-    content: string
-    type: string
-    emoji: string
-  }[]
   status?: string
-  createdAt: string
+  createdAt: Date | null
+  readAt?: Date | null
 }
 
-type Message = {
-  messageId: number
-  receiverId: number
-  receiverNickname: string
-  content: string
-  status?: string
-  createdAt: string
-}
 // 받은 쪽지 리스트 조회 타입
 export interface MessageResponse {
-  received_message_count: number
-  nextCursor: string | null
+  receivedMessageCount?: number
+  sentMessageCount?: number
+  nextCursor: Date | null
   messageList: Message[]
 }
 export interface useGetMessageListProps {
@@ -79,19 +53,20 @@ export interface useGetMessageListProps {
   order?: string
 }
 
-type Item = {
-  id: number
-  emoji: string
-  name: string
-}
-
-export type DataType = {
-  감사: Item[]
-  사과: Item[]
-  응원: Item[]
-  화해: Item[]
-}
-
 export type EmojiProps = {
-  category: keyof DataType
+  templateId: string
+  type: string
+  content: string
+  emoji: string
+}
+
+export type EmojiResponse = {
+  status: number
+  message: string
+  data: {
+    templateId: string
+    type: string
+    content: string
+    emoji: string
+  }
 }
--- src/features/messagebox/ui/ReactionPage.tsx
+++ src/features/messagebox/ui/ReactionPage.tsx
@@ -3,9 +3,11 @@ import { useRouter } from 'next/navigation'
 import { Button } from '@/shared/components/Button'
 import { EmojiProps } from '@/features/messagebox/model/messagebox.types'
 import { MessageType } from '@/features/messagebox/_detail/model/messagebox.types'
-import ReactionList from '@/features/messagebox/ui/ReactionList'
+// import ReactionList from '@/features/messagebox/ui/ReactionList'
 import { useGetEmoji } from '../_detail/api/detailQuery'
-
+import { useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
+import Image from 'next/image'
 export default function ReactionPage({
   messageType,
   userId,
@@ -16,9 +18,11 @@ export default function ReactionPage({
   messageId: number
 }) {
   const lists = ['감사', '사과', '응원', '화해']
+  const [selected, setSelected] = useState<string[]>([])
   const router = useRouter()
   const onSubmit = () => {
     // 데이터 보내는 로직 추가
+    // console.log(...selected)
     router.push(`/messagebox/${userId}/${messageType}/${messageId}`)
   }
 
@@ -28,10 +32,9 @@ export default function ReactionPage({
   if (error) return <div>Error fetching emojis.</div>
   if (!data) return null
 
-  // 데이터 그룹화
   const groupedData: Record<EmojiProps['type'], EmojiProps[]> = lists.reduce(
     (acc, type) => {
-      acc[type] = [] // 초기화
+      acc[type] = []
       return acc
     },
     {} as Record<EmojiProps['type'], EmojiProps[]>
@@ -39,35 +42,83 @@ export default function ReactionPage({
 
   data.forEach((item) => {
     if (groupedData[item.type]) {
-      groupedData[item.type].push(item) // 해당 유형의 배열에 추가
+      groupedData[item.type].push(item)
     }
   })
 
+  const storedData = (e: string) => {
+    if (selected.includes(e)) {
+      setSelected(selected.filter((v) => v !== e))
+    } else if (selected.length < 5) {
+      setSelected([...selected, e])
+    } else {
+      toast('반응은 최대 5개 보낼 수 있어요', {
+        icon: (
+          <Image
+            src='/toast_alert.svg'
+            alt='toast_alert'
+            width={18}
+            height={18}
+          />
+        ),
+        style: {
+          borderRadius: '16px',
+          background: '#474747',
+          color: '#fff',
+          width: '100%',
+          height: '56px',
+          marginBottom: '60px',
+          paddingLeft: '1.5rem',
+          paddingRight: '1.5rem',
+        },
+      })
+    }
+  }
   return (
     <div className='w-full h-full'>
       {lists.map((type) => (
         <div
           key={type}
-          className='w-full flex flex-col py-[20px] h-full border-b-[6px] border-b-gray-1'
+          className='w-full flex flex-col py-[20px] h-full border-b-[6px] border-b-gray-1 px-6'
         >
           <h2 className='text-base font-semibold tracking-[-0.75px]'>{type}</h2>
           <div className='flex w-full flex-wrap gap-2 pt-[20px]'>
             {groupedData[type].map((item) => (
-              <div key={item.templateId}>
-                <ReactionList data={item} />
+              <div
+                key={item.templateId}
+                onClick={() => storedData(item.templateId)}
+              >
+                <div className='w-full flex h-full'>
+                  <div className='flex-none'>
+                    <div
+                      className={`${selected.includes(item.templateId) ? 'selected-emoji' : 'bg-white border-[#C5C5C5]'} 
+    w-full p-[12px] h-[35px] text-[21px] rounded-[60px] flex items-center cursor-pointer 
+    border-[0.5px] hover:bg-[#35B6FF] hover:bg-opacity-40 hover:border-[#35B6FF] gap-[4px]`}
+                    >
+                      {item.emoji}
+                      <div className='w-full flex text-[15px] font-semibold leading-[68.1%] tracking-[-2%]'>
+                        {item.content}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
         </div>
       ))}
-
-      <Button
-        type='button'
-        onClick={onSubmit}
-        className='h-fit p-4 bg-[#35B6FF] text-white rounded-2xl font-bold w-full'
-      >
-        완료
-      </Button>
+      <div className='fixed flex w-full max-w-[390px] justify-center items-center bottom-0 pb-[10px] z-10 px-6'>
+        <Toaster position='bottom-center' reverseOrder={false} />
+        <div className='relative flex items-center justify-center mt-[46px] w-full h-fit '>
+          <Button
+            type='button'
+            onClick={onSubmit}
+            className='h-fit p-4 bg-[#35B6FF] rounded-2xl w-full'
+          >
+            <div className='text-[18px] text-white font-bold '>완료</div>
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
