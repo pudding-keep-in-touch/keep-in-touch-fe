17e81940984ab728ff1e6f4ecb11b09d082dc21a
jieunw <jieunwang00@gmail.com>
2024-12-05 20:20:43 +0900

    fix: 메인 쪽지함에서도 페이지네이션이 되던 오류 수정

--- src/features/messagebox/ui/ViewAllMessage.tsx
+++ src/features/messagebox/ui/ViewAllMessage.tsx
@@ -1,9 +1,7 @@
 'use client'
 import { MessageType } from '@/features/messagebox/_detail/model/messagebox.types'
-import MessagesBlock from '@/features/messagebox/ui/MessagesBlock'
-
+import ViewThreeMessages from '@/features/messagebox/ui/ViewThreeMessages'
 export default function ViewAllMessage({
-  messageType,
   userId,
 }: {
   messageType: MessageType
@@ -12,11 +10,11 @@ export default function ViewAllMessage({
   return (
     <div className='max-w-[390px] w-full'>
       <div className='w-full pb-6'>
-        <MessagesBlock messageType='received' userId={userId} limit={3} />
+        <ViewThreeMessages messageType='received' userId={userId} />
       </div>
       <div className='border-b-[6px] border-b-white w-full mb-2'></div>
       <div className='w-full pb-6'>
-        <MessagesBlock messageType='sent' userId={userId} limit={3} />
+        <ViewThreeMessages messageType='sent' userId={userId} />
       </div>
     </div>
   )
---
+++ src/features/messagebox/ui/ViewThreeMessages.tsx
@@ -0,0 +1,77 @@
+'use client'
+import { useEffect } from 'react'
+import { useGetMessageList } from '@/features/messagebox/_detail/api/detailQuery'
+import { MessageType } from '@/features/messagebox/_detail/model/messagebox.types'
+import MessageList from '@/features/messagebox/ui/MessageList'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Spinner } from '@/shared/components/Sppiner'
+
+export default function MessagesBlock({
+  messageType,
+  userId,
+}: {
+  messageType: MessageType
+  userId: number
+}) {
+  const { data, isLoading, isError, error } = useGetMessageList({
+    userId,
+    type: messageType,
+    cursor: null,
+    limit: 3,
+    order: 'desc',
+  })
+
+  useEffect(() => {
+    if (isError) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const errorStatus = (error as any)?.response?.status
+      console.error(`Error fetching messages. Status: ${errorStatus}`)
+    }
+  }, [isError, error])
+
+  const messageCount =
+    messageType === 'sent'
+      ? data?.sentMessageCount || 0
+      : data?.receivedMessageCount || 0
+
+  const moreLink = messageCount > 3 && (
+    <Link
+      href={`/messagebox/${userId}/${messageType}`}
+      className='flex items-center gap-[9px]'
+    >
+      <p className='text-[#6B7684]'>더보기</p>
+      <Image src='/nav_icon.svg' alt='watch more' width={18} height={18} />
+    </Link>
+  )
+
+  const description =
+    messageType === 'sent' ? '아직 보낸 퐁이 없어요!' : '아직 받은 퐁이 없어요!'
+
+  return (
+    <div className='max-w-[390px] px-6 w-full min-h-[368px]'>
+      <div className='w-full h-[67px] flex items-center justify-between gap-1'>
+        <h2 className='font-semibold text-[18px] flex items-center gap-2'>
+          <div>{messageType === 'sent' ? '보낸 퐁' : '받은 퐁'}</div>
+          <div>({messageCount})</div>
+        </h2>
+        {moreLink}
+      </div>
+      <div>
+        {isLoading && <Spinner />}
+        {messageCount > 0 ? (
+          <MessageList
+            messages={data}
+            userId={userId}
+            messageType={messageType}
+          />
+        ) : (
+          <div className='flex flex-col items-center text-[#333D4B] pb-[162px] pt-[110px]'>
+            <Image src='/no_msg.svg' alt='home_icon' width={65} height={57} />
+            <p className='leading-none pt-[11px] text-[17px]'>{description}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
