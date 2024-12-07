ca8d6b895ea634ae843ca81ad3171c039a33a20c
lunashp <hijnshp@gmail.com>
2024-12-07 14:04:28 +0900

    fix: 불필요한 데이터 주석

--- src/entities/questions/questionData.ts
+++ src/entities/questions/questionData.ts
@@ -1,55 +1,55 @@
-import { questionsType } from '@/entities/questions/questionType'
-import { questionType } from '@/entities/questions/questionType'
+// import { questionsType } from '@/entities/questions/questionType'
+// import { questionType } from '@/entities/questions/questionType'
 
-export const questions: questionsType = [
-  {
-    questionId: '1',
-    userId: '1',
-    content: '솔직한 마음을 말해줘',
-    createdAt: '2024-09-15T10:30:00Z',
-  },
-  {
-    questionId: '2',
-    userId: '1',
-    content: '솔직한 마음을 말해줘2',
-    createdAt: '2024-09-15T10:30:00Z',
-  },
-  {
-    questionId: '3',
-    userId: '1',
-    content: '솔직한 마음을 말해줘4',
-    createdAt: '2024-09-15T10:30:00Z',
-  },
-  {
-    questionId: '4',
-    userId: '1',
-    content: '솔직한 마음을 말해줘5',
-    createdAt: '2024-09-15T10:30:00Z',
-  },
-  {
-    questionId: '5',
-    userId: '1',
-    content: '솔직한 마음을 말해줘6',
-    createdAt: '2024-09-15T10:30:00Z',
-  },
-  {
-    questionId: '6',
-    userId: '1',
-    content: '솔직한 마음을 말해줘7',
-    createdAt: '2024-09-15T10:30:00Z',
-  },
-  {
-    questionId: '7',
-    userId: '1',
-    content: '솔직한 마음을 말해줘8',
-    createdAt: '2024-09-15T10:30:00Z',
-  },
-]
+// export const questions: questionsType = [
+//   {
+//     questionId: '1',
+//     userId: '1',
+//     content: '솔직한 마음을 말해줘',
+//     createdAt: '2024-09-15T10:30:00Z',
+//   },
+//   {
+//     questionId: '2',
+//     userId: '1',
+//     content: '솔직한 마음을 말해줘2',
+//     createdAt: '2024-09-15T10:30:00Z',
+//   },
+//   {
+//     questionId: '3',
+//     userId: '1',
+//     content: '솔직한 마음을 말해줘4',
+//     createdAt: '2024-09-15T10:30:00Z',
+//   },
+//   {
+//     questionId: '4',
+//     userId: '1',
+//     content: '솔직한 마음을 말해줘5',
+//     createdAt: '2024-09-15T10:30:00Z',
+//   },
+//   {
+//     questionId: '5',
+//     userId: '1',
+//     content: '솔직한 마음을 말해줘6',
+//     createdAt: '2024-09-15T10:30:00Z',
+//   },
+//   {
+//     questionId: '6',
+//     userId: '1',
+//     content: '솔직한 마음을 말해줘7',
+//     createdAt: '2024-09-15T10:30:00Z',
+//   },
+//   {
+//     questionId: '7',
+//     userId: '1',
+//     content: '솔직한 마음을 말해줘8',
+//     createdAt: '2024-09-15T10:30:00Z',
+//   },
+// ]
 
-export const question: questionType = {
-  questionId: '1',
-  userId: '1',
-  content: '솔직한 마음을 말해줘',
-  isHidden: false,
-  createdAt: '2024-09-15T10:30:00Z',
-}
+// export const question: questionType = {
+//   questionId: '1',
+//   userId: '1',
+//   content: '솔직한 마음을 말해줘',
+//   isHidden: false,
+//   createdAt: '2024-09-15T10:30:00Z',
+// }
