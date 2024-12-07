8df341ef75460b310dfcf0e275fb8b501e27d559
lunashp <hijnshp@gmail.com>
2024-12-07 14:04:50 +0900

    feat: callback 로직 수정, useEffect 분리

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,41 +1,51 @@
 'use client'
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import MainLayout from '@/shared/ui/layouts/MainLayout'
 
 export default function Callback() {
   const router = useRouter()
   const searchParams = useSearchParams()
+  const [redirectUrl, setRedirectUrl] = useState<string | null>(null)
 
-  React.useEffect(() => {
+  useEffect(() => {
     const token = searchParams.get('accessToken')
     const userId = searchParams.get('userId')
     const storedRedirectUrl = localStorage.getItem('redirect_before_login') // 로컬스토리지 값
 
     // redirectUrl 결정
-    const redirectUrl =
+    const calculatedRedirectUrl =
       searchParams.get('redirectUrl') || // URL에 있는 경우
       storedRedirectUrl || // 로컬스토리지 값
       (userId ? `/home/${userId}` : `/login`) // 기본값
 
-    // 디버깅 코드 : 삭제예정
+    setRedirectUrl(calculatedRedirectUrl)
+
+    // 디버깅 코드 : 삭제 예정
     console.log('searchParams redirectUrl:', searchParams.get('redirectUrl'))
     console.log(
       'localStorage redirect_before_login:',
       localStorage.getItem('redirect_before_login')
     )
-    console.log('redirectUrl 결정:', redirectUrl)
+    console.log('redirectUrl 결정:', calculatedRedirectUrl)
+  }, [searchParams])
 
-    if (token && userId) {
-      localStorage.setItem('keep_in_touch_token', token)
-      localStorage.setItem('keep_in_touch_user_id', userId)
-      localStorage.removeItem('redirect_before_login') // 초기화
-      router.push(decodeURIComponent(redirectUrl)) // 최종 redirectUrl로 이동
-    } else {
-      router.push('/login') // 토큰 또는 userId가 없으면 /login으로 이동
+  useEffect(() => {
+    if (redirectUrl) {
+      const token = searchParams.get('accessToken')
+      const userId = searchParams.get('userId')
+
+      if (token && userId) {
+        localStorage.setItem('keep_in_touch_token', token)
+        localStorage.setItem('keep_in_touch_user_id', userId)
+        localStorage.removeItem('redirect_before_login') // 초기화
+        router.push(decodeURIComponent(redirectUrl)) // 최종 redirectUrl로 이동
+      } else {
+        router.push('/login') // 토큰 또는 userId가 없으면 /login으로 이동
+      }
     }
-  }, [searchParams, router])
+  }, [redirectUrl, searchParams, router])
 
   return (
     <MainLayout>
