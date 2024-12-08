2c6ee5d7837e6dd3a718e3a829169228d38b2475
JiyeonMin <ichbinmin2@gmail.com>
2024-12-08 12:44:40 +0900

    fix : 로그인 페이지에 스플래쉬 스텝 렌더 추가

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,13 +1,48 @@
+'use client'
+
 import React from 'react'
+import splashJson from '@/features/main/lottie/splash.json'
+import Lottie from 'lottie-react'
 
 interface Props {
   children: React.ReactNode
 }
 
 export default function Layout({ children }: Props) {
-  return (
-    <div className='w-full min-h-screen flex flex-col items-center bg-white border-l border-r border-[#D0E4FF] box-border'>
-      {children}
-    </div>
-  )
+  const [step, setStep] = React.useState<number>(1)
+
+  const renderStep = (step: number) => {
+    switch (step) {
+      case 1:
+        return (
+          <div className='w-full min-h-screen flex flex-col items-center bg-white border-l border-r border-[#D0E4FF] box-border'>
+            <Lottie
+              className='lottie-player'
+              animationData={splashJson}
+              loop={0}
+            />
+          </div>
+        )
+
+      case 2:
+        return (
+          <div className='w-full min-h-screen flex flex-col items-center bg-white border-l border-r border-[#D0E4FF] box-border'>
+            {children}
+          </div>
+        )
+    }
+  }
+
+  React.useEffect(() => {
+    if (step === 1) {
+      const timer = setTimeout(() => setStep(2), 2000)
+
+      // 클린업 함수
+      return () => clearTimeout(timer)
+    }
+  }, [step])
+
+  console.log('step', step)
+
+  return <>{renderStep(step)}</>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,36 @@
 'use client'
-import Image from 'next/image'
-import MainLayout from '@/shared/ui/layouts/MainLayout'
+
 import { useRouter } from 'next/navigation'
 import React from 'react'
-import splashJson from '@/features/main/lottie/splash.json'
-import Lottie from 'lottie-react'
 
 export default function Main() {
   const router = useRouter()
 
   // TODO : 로그인 붙이고 처리 예정
-  // useEffect(() => {
-  //   const checkToken = async () => {
-  //     const token =
-  //       typeof window !== 'undefined'
-  //         ? localStorage.getItem('keep_in_touch_token')
-  //         : null
-  //     const userId =
-  //       typeof window !== 'undefined'
-  //         ? localStorage.getItem('keep_in_touch_user_id')
-  //         : null
-
-  //     if (!token) {
-  //       setTimeout(() => {
-  //         router.push('/login')
-  //       }, 2000)
-  //       return
-  //     }
-  //     try {
-  //       const response = await fetch(
-  //         `${process.env.NEXT_PUBLIC_API_URL}/user/${userId}/home`,
-  //         {
-  //           headers: {
-  //             Authorization: `Bearer ${token}`,
-  //           },
-  //         }
-  //       )
-  //       if (!response.ok) {
-  //         setTimeout(() => router.push('/login'), 2000)
-  //       } else {
-  //         setTimeout(() => router.push('/home'), 2000)
-  //       }
-  //     } catch (error) {
-  //       console.error(error)
-  //       setTimeout(() => router.push('/login'), 2000)
-  //     }
-  //   }
-  //   checkToken()
-  // }, [router])
-
   React.useEffect(() => {
-    setTimeout(() => router.push('/login'), 2000)
-  })
+    const checkToken = async () => {
+      const token =
+        typeof window !== 'undefined'
+          ? localStorage.getItem('keep_in_touch_token')
+          : null
+      const userId =
+        typeof window !== 'undefined'
+          ? localStorage.getItem('keep_in_touch_user_id')
+          : null
 
+      if (!token) {
+        setTimeout(() => {
+          router.push('/login')
+        }, 2000)
+        return
+      }
+      router.push(`/home/${userId}`)
+    }
+    checkToken()
+  }, [router])
+
+  // TODO : 로딩 스피너 추가 예정
   return (
-    <MainLayout>
-      <Lottie className='lottie-player' animationData={splashJson} loop={0} />
-    </MainLayout>
+    <div className='w-full min-h-screen flex flex-col items-center bg-white border-l border-r border-[#D0E4FF] box-border'></div>
   )
 }
