ca2263d67fabf26aa3f9c803cad1b75fc50db37a
jieunw <jieunwang00@gmail.com>
2024-12-09 20:50:07 +0900

    fix: QuestionBox CSS 수정

--- src/features/messagebox/_detail/ui/component/QuestionBox.tsx
+++ src/features/messagebox/_detail/ui/component/QuestionBox.tsx
@@ -6,10 +6,10 @@ interface QuestionBoxProps {
 const QuestionBox: React.FC<QuestionBoxProps> = ({ content }) => {
   return (
     <div className='bg-white border-[1px] rounded-2xl flex flex-col w-full items-center'>
-      <h3 className='text-sm font-semibold text-[#333D4B] bg-gray-1 w-full text-center py-3 rounded-2xl'>
+      <h3 className='text-sm font-semibold text-[#333D4B] bg-gray-1 w-full text-center py-3 rounded-t-2xl'>
         질문
       </h3>
-      <p className='text-sm text-[#6B7684] text-center my-2.5 mx-11'>
+      <p className='rounded-2xl text-sm text-[#6B7684] text-center my-2.5 mx-11'>
         {content}
       </p>
     </div>
