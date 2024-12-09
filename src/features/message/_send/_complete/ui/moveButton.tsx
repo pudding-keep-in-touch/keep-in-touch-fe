4c11ced390c7e986fca26fb187fc19d8660a4f89
lunashp <hijnshp@gmail.com>
2024-12-09 15:06:43 +0900

    fix: 타입 전송 문구 수정

--- src/features/message/_send/_complete/ui/moveButton.tsx
+++ src/features/message/_send/_complete/ui/moveButton.tsx
@@ -1,30 +1,30 @@
-'use client'
+// 'use client'
 
-import { Button } from '@/shared/components/Button'
-import { useRouter } from 'next/navigation'
+// import { Button } from '@/shared/components/Button'
+// import { useRouter } from 'next/navigation'
 
-export default function MoveButton({
-  dmId,
-  variety,
-}: {
-  dmId: string
-  variety: string
-}) {
-  const userId =
-    typeof window !== 'undefined'
-      ? localStorage.getItem('keep_in_touch_user_id')
-      : null
-  const router = useRouter()
-  const moveHandler = () => {
-    router.push(`/message/sent/${userId}/${variety}/${dmId}?base=sent`)
-  }
-  return (
-    <Button
-      type='button'
-      className='h-fit p-4 bg-[#35B6FF] text-lg text-white rounded-2xl font-bold w-full'
-      onClick={moveHandler}
-    >
-      보낸 쪽지 확인하기
-    </Button>
-  )
-}
+// export default function MoveButton({
+//   dmId,
+//   variety,
+// }: {
+//   dmId: string
+//   variety: string
+// }) {
+//   const userId =
+//     typeof window !== 'undefined'
+//       ? localStorage.getItem('keep_in_touch_user_id')
+//       : null
+//   const router = useRouter()
+//   const moveHandler = () => {
+//     router.push(`/message/sent/${userId}/${variety}/${dmId}?base=sent`)
+//   }
+//   return (
+//     <Button
+//       type='button'
+//       className='h-fit p-4 bg-[#35B6FF] text-lg text-white rounded-2xl font-bold w-full'
+//       onClick={moveHandler}
+//     >
+//       보낸 쪽지 확인하기
+//     </Button>
+//   )
+// }
