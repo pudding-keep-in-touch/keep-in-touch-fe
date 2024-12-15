489e5731d04ca8ec2e31fc38eb65e796eebd59e0
lunashp <hijnshp@gmail.com>
2024-12-15 15:11:41 +0900

    fix: complete 페이지 버튼 위치 수정

--- src/features/questions/ui/CompletePage.tsx
+++ src/features/questions/ui/CompletePage.tsx
@@ -5,7 +5,7 @@ export default function CompletePage() {
   return (
     <>
       <div className='flex flex-col min-h-screen items-center'>
-        <div className='mt-[250px] flex flex-col w-[100%] items-center text-center'>
+        <div className='mt-[280px] flex flex-col w-[100%] items-center text-center'>
           <Image src='/send.svg' alt='send' width={80} height={80} />
           <h2 className='text-[#333D4B] text-l font-bold leading-[150%] mt-6 mb-60'>
             친구에게 퐁 보내기 완료!
