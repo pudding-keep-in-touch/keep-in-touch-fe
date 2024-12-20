2a4883d4daece3c48a9b3e67449fb9a4cf35ce91
JiyeonMin <ichbinmin2@gmail.com>
2024-12-20 19:10:01 +0900

    fix : home에서 쿠키로 관리되는 유저 아이디가 사용자의 아이디와 동일하지 않을시, 접근 불가능하도록 분기처리함

--- src/app/home/[userId]/page.tsx
+++ src/app/home/[userId]/page.tsx
@@ -1,9 +1,26 @@
+'use client'
+
 import Home from '@/features/home/home'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { useCookies } from 'react-cookie'
 
 export default function HomePage({
   params: { userId },
 }: {
   params: { userId: string }
 }) {
+  const router = useRouter()
+  const [cookies] = useCookies(['keep_in_touch_token', 'keep_in_touch_user_id'])
+
+  React.useEffect(() => {
+    const cookiesToken = cookies.keep_in_touch_token
+    const cookiesUserId = cookies.keep_in_touch_user_id
+
+    if (!cookiesToken || cookiesUserId !== userId) {
+      router.replace('/') // TODO : 인증 실패 시 Unauthorized로 리다이렉트
+    }
+  }, [cookies, userId, router])
+
   return <Home userId={userId} />
 }
