6d937594364160431ed4607c59e073e4c0c9cec7
JiyeonMin <ichbinmin2@gmail.com>
2024-12-21 12:35:45 +0900

    Revert "test : 인증 관련 로직 디버깅3"
    This reverts commit bc456d43f7ec83bb81e8258a98c5dd1e5b6747ab.

--- src/features/auth/context/AuthProvider.tsx
+++ src/features/auth/context/AuthProvider.tsx
@@ -34,13 +34,11 @@ export default function AuthProvider({ children }: AuthProviderProps) {
   const isTokenValid = (token: string): boolean => {
     try {
       const { exp } = decodeJwt(token)
-      const currentTime = Math.floor(Date.now() / 1000)
-
       console.log('AuthContext > Decoded exp:', exp)
+      const currentTime = Math.floor(Date.now() / 1000)
       console.log('AuthContext > Current time:', currentTime)
 
-      // exp가 숫자인지 확인 후 비교
-      return typeof exp === 'number' && exp > currentTime
+      return !(exp && exp < currentTime)
     } catch (error) {
       console.error('AuthProvider > Error decoding JWT:', error)
       return false
@@ -48,8 +46,6 @@ export default function AuthProvider({ children }: AuthProviderProps) {
   }
 
   const handleLogout = () => {
-    console.log('AuthProvider > Logging out and navigating to /login')
-
     removeCookie('keep_in_touch_token', { path: '/' })
     removeCookie('keep_in_touch_user_id', { path: '/' })
     setAuthState({ isLoggedIn: false, isLoading: false, userId: null })
@@ -57,28 +53,31 @@ export default function AuthProvider({ children }: AuthProviderProps) {
   }
 
   const checkAuth = async () => {
-    const accessToken = cookies.keep_in_touch_token
-    const userId = cookies.keep_in_touch_user_id
-    const redirectUrl = localStorage.getItem('redirect_before_login') || null
+    try {
+      const accessToken = cookies.keep_in_touch_token
+      const userId = cookies.keep_in_touch_user_id
+      const redirectUrl = localStorage.getItem('redirect_before_login') || null
 
-    console.log('AuthProvider > accessToken:', accessToken)
-    console.log('AuthProvider > userId:', userId)
+      console.log('AuthProvider > accessToken', accessToken)
+      console.log('AuthProvider > userId', userId)
 
-    if (!accessToken || !isTokenValid(accessToken)) {
-      console.warn('AuthProvider > Invalid or expired token')
+      if (!accessToken || !isTokenValid(accessToken)) {
+        console.log('AuthProvider > !accessToken || !isTokenValid(accessToken')
+        handleLogout()
+        throw new Error('Invalid or expired token')
+      }
+
+      setAuthState({ isLoggedIn: true, isLoading: false, userId })
+
+      if (redirectUrl) {
+        localStorage.removeItem('redirect_before_login')
+        router.replace(redirectUrl)
+      }
+    } catch (error) {
+      console.error(error)
+      console.log('AuthProvider > error', error)
       toast.error('세션이 만료되었습니다. 다시 로그인해주세요.')
       handleLogout()
-      return
-    }
-
-    // 토큰 유효한 경우
-    setAuthState({ isLoggedIn: true, isLoading: false, userId })
-
-    // 이전 경로가 있는 경우 이동
-    if (redirectUrl) {
-      localStorage.removeItem('redirect_before_login')
-      console.log(`AuthProvider > Redirecting to saved URL: ${redirectUrl}`)
-      router.replace(redirectUrl)
     }
   }
 
@@ -86,11 +85,7 @@ export default function AuthProvider({ children }: AuthProviderProps) {
     if (authState.isLoading) {
       checkAuth()
     }
-  }, [
-    authState.isLoading,
-    cookies.keep_in_touch_token,
-    cookies.keep_in_touch_user_id,
-  ])
+  }, [authState.isLoading])
 
   const value = React.useMemo(
     () => ({
@@ -102,7 +97,7 @@ export default function AuthProvider({ children }: AuthProviderProps) {
   )
 
   if (authState.isLoading) {
-    return <div>AuthProvider Loading...</div>
+    return <div>Loading...</div>
   }
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
